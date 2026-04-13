385e61a4409762753e217a5ebab6044cd4f1fd3f
jackiejua <119469167+jackiejua@users.noreply.github.com>
2026-04-13 01:03:05 -0600

    form change

--- tackie/src/components/AddToDoForm.jsx
+++ tackie/src/components/AddToDoForm.jsx
@@ -84,7 +84,7 @@ function AddTodoForm({ onClose }) {
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          placeholder="e.g., PWA Final Project Report"
+          placeholder="e.g., Special Topics Presentation..."
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
